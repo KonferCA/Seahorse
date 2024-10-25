86c3c11c8588ca7be108dec3840e365701e51805
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-10-24 22:09:19 -0400

    Add types

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,29 @@
 'use client';
-import { CreateMLCEngine } from '@mlc-ai/web-llm';
-import { useSearchParams } from 'next/navigation';
+import { CreateMLCEngine, InitProgressCallback } from '@mlc-ai/web-llm';
 import { useState } from 'react';
 
-// Callback function to update model loading progress
-const initProgressCallback = (initProgress) => {
-    console.log(initProgress);
-};
+const initProgressCallback: InitProgressCallback = (report) => {
+    console.log(report);
+}
+
 const selectedModel = 'Llama-3.2-1B-Instruct-q4f32_1-MLC';
 
 const engine = await CreateMLCEngine(
     selectedModel,
-    { initProgressCallback: initProgressCallback } // engineConfig
+    { initProgressCallback: initProgressCallback }, 
 );
-
-const messages = [
-    { role: 'system', content: 'You are a helpful AI assistant.' },
-];
 
 export default function Home() {
     const [prompt, setPrompt] = useState('');
 
     const query = async () => {
         const reply = await engine.chat.completions.create({
-            messages: [...messages, { role: 'user', content: prompt }],
+            messages: [
+                { role: 'system', content: 'You are a helpful AI assistant.' },
+                { role: 'user', content: prompt },
+            ],
         });
+
         console.log(reply);
     };
 
